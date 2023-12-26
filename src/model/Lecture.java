d91211509cccf816b10665674476cfba2455b6cb
JinseoKim <eunoia@handong.ac.kr>
2023-12-27 04:12:48 +0900

    231227: DateTimeFormatter 사용

--- src/model/Lecture.java
+++ src/model/Lecture.java
@@ -2,15 +2,28 @@ package model;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Lecture {
     private String title;
     private String lecturer;
     private String tag;
     private double stars;
+    private String regDate;
+
+    public Lecture(String title, String lecturer, String tag, double stars) {
+        this.title = title;
+        this.lecturer = lecturer;
+        this.tag = tag;
+        this.stars = stars;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.regDate = LocalDateTime.now().format(formatter);
+    }
 
     @Override
     // Object를 String으로 변환 시 출력할 문자열 정의
