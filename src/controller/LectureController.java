cb4bf772275fb516fece7a08c4d26d508cf88847
JinseoKim <eunoia@handong.ac.kr>
2023-12-27 04:19:06 +0900

    231227: controller 주석 완료

--- src/controller/LectureController.java
+++ src/controller/LectureController.java
@@ -8,23 +8,28 @@ import java.util.List;
 public class LectureController {
     private final ArrayList<Lecture> lectures = new ArrayList<>();
 
+    // 강의명 중복 확인
     public boolean isLectureExist(String title) {
         return lectures.stream().anyMatch(lecture -> lecture.getTitle().equals(title));
     }
 
+    // 번호 범위 확인
     public boolean isIndexExist(int id) {
         return 0 >= id || id > lectures.size();
     }
 
+    // 입력받은 정보의 강의 생성
     public void createLecture(String title, String lecturer, String tag, double stars) {
         Lecture newLecture = new Lecture(title, lecturer, tag, stars);
         lectures.add(newLecture);
     }
 
+    // 전체 강의 읽어오기
     public ArrayList<Lecture> listAllLectures() {
         return new ArrayList<>(lectures);
     }
 
+    // parameter들이 비어 있지 않을 때, 강의 정보 업데이트
     public void updateLecture(int id, String title, String lecturer, String tag, double stars) {
         if (!title.isEmpty()) lectures.get(id-1).setTitle(title);
         if (!lecturer.isEmpty()) lectures.get(id-1).setLecturer(lecturer);
@@ -32,10 +37,12 @@ public class LectureController {
         lectures.get(id-1).setStars(stars);
     }
 
+    // parameter의 index의 강의 삭제
     public void deleteLecture(int id) {
         lectures.remove(id-1);
     }
 
+    // parameter의 강의명이 포함된 강의 검색
     public List<Lecture> searchLectures(String title) {
         return lectures.stream().filter(lecture -> lecture.getTitle().contains(title)).toList();
     }
